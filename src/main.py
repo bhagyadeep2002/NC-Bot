4816425e0796d212cb8773d57dbe44b2e31bb847
bhagyadeep2002 <pbhagyadeep@gmail.com>
2022-08-04 14:07:50 +0530

    added command handlers and dispatcher for animal commands in main.py

--- src/main.py
+++ src/main.py
@@ -11,6 +11,9 @@ from telegram.ext import CallbackContext
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 from commands.weather import get_weather
+from commands.animals import getCat
+from commands.animals import getDog
+from commands.animals import getHamster
 load_dotenv()
 
 updater = Updater(token=os.environ["TG_BOT"])
@@ -25,10 +28,16 @@ def start(update: Update, context: CallbackContext):
 def unknown(update: Update, context: CallbackContext):
     update.message.reply_text(text="Sorry I did not recognise that command")
 
+cat_handler = CommandHandler('cat',getCat)
+hamster_handler = CommandHandler('hamster',getHamster)
+dog_handler = CommandHandler('dog',getDog)
 weather_handler = CommandHandler('weather',get_weather)
 start_handler = CommandHandler('start',start)
 unknown_handler = MessageHandler(Filters.command, unknown)
 
+dispatcher.add_handler(hamster_handler)
+dispatcher.add_handler(dog_handler)
+dispatcher.add_handler(cat_handler)
 dispatcher.add_handler(weather_handler)
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(unknown_handler)
